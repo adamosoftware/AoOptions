c16413d486c82bc53c71e6790b21de91e12f1548
Adam O'Neil <adamosoftware@gmail.com>
2017-01-02 12:08:51 -0500

    removed unnecessary usings

--- OptionsTest/Options.cs
+++ OptionsTest/Options.cs
@@ -1,10 +1,4 @@
 ﻿using AdamOneilSoftware;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OptionsTest
 {
